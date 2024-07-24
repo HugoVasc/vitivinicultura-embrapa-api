33be98951d3d0b0db8b0b837d0695542c07c05b5
Hugo Vasconcelos <hugo.svasc@gmail.com>
2024-07-23 22:50:10 -0300

    Trazendo dados do BD referentes ao processamento das uvas. Versão inicial da resposta

--- app/routes/processamento.py
+++ app/routes/processamento.py
@@ -1,5 +1,10 @@
 from fastapi import APIRouter
+from typing import List
 from starlette import status
+from sqlalchemy.ext.asyncio import AsyncSession
+from ..database import SessionLocal
+
+from ..models import GrapeVarieties, GrapeCategories, GrapeSubCategories, ProcessedGrapes
 
 router = APIRouter(
     prefix="/processamento",
@@ -11,8 +16,32 @@ async def processamento():
     """
     Rota Default para /processamento
     """
+    # JOIN com GrapeVarieties, GrapeCategories e GrapeSubCategories e ProcessedGrapes
+    result = SessionLocal().query(
+        GrapeVarieties,
+        GrapeCategories,
+        GrapeSubCategories,
+        ProcessedGrapes
+    ).join(
+        GrapeCategories, GrapeVarieties.id == GrapeCategories.variety_id
+    ).join(
+        GrapeSubCategories, GrapeCategories.id == GrapeSubCategories.category_id
+    ).join(
+        ProcessedGrapes, GrapeSubCategories.id == ProcessedGrapes.subcategory_id
+    ).all()
+    result_list = []
+    for item in result:
+        result_dict = {
+            "GrapeVarieties": item[0].__dict__,
+            "GrapeCategories": item[1].__dict__,
+            "GrapeSubCategories": item[2].__dict__,
+            "ProcessedGrapes": item[3].__dict__,
+        }
+        # Removendo '_sa_instance_state' de cada dicionário
+        for key in result_dict:
+            result_dict[key].pop('_sa_instance_state', None)
+        result_list.append(result_dict)
 
-    return {
-        'message': 'OK',
-        'env': 'processamento'
-    }
+    return result_list
+
+
