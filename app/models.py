989f774f188e9a9f2d68a27f131fe5c582f5df0e
Hugo Vasconcelos <hugo.svasc@gmail.com>
2024-07-23 22:53:56 -0300

    Revert "Ajustando declaração das colunas de chave estrangeira para corrigir a criação das tables"
    This reverts commit 6f4bb16ba5ae2d107c8a220a2fd683c27e27b01e.

--- app/models.py
+++ app/models.py
@@ -14,13 +14,13 @@ class WineSubCategories(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     description = Column(String, nullable=False)
-    category_id = Column(Integer, ForeignKey("categories.id"))
+    category_id = ForeignKey("Categories.id")
 
 
 class ProducedWineCategoriesWithQuantity(Base):
     __tablename__ = "categories_with_quantity"
     id = Column(Integer, primary_key=True, index=True)
-    category_id = Column(Integer, ForeignKey("categories.id"))
+    category_id = ForeignKey("Categories.id")
     year = Column(Integer, nullable=False)
     quantity_l = Column(Integer, nullable=False)
 
@@ -28,7 +28,7 @@ class ProducedWineCategoriesWithQuantity(Base):
 class ProducedWineSubCategoriesWithQuantity(Base):
     __tablename__ = "sub_categories_with_quantity"
     id = Column(Integer, primary_key=True, index=True)
-    subcategory_id = Column(Integer, ForeignKey("sub_categories.id"))
+    subcategory_id = ForeignKey("SubCategories.id")
     year = Column(Integer, nullable=False)
     quantity_l = Column(Integer, nullable=False)
 
@@ -36,7 +36,7 @@ class ProducedWineSubCategoriesWithQuantity(Base):
 class ComercializedWineCategoriesWithQuantity(Base):
     __tablename__ = "comercialized_categories_with_quantity"
     id = Column(Integer, primary_key=True, index=True)
-    category_id = Column(Integer, ForeignKey("categories.id"))
+    category_id = ForeignKey("Categories.id")
     year = Column(Integer, nullable=False)
     quantity_l = Column(Integer, nullable=False)
 
@@ -44,7 +44,7 @@ class ComercializedWineCategoriesWithQuantity(Base):
 class ComercializedWineSubCategoriesWithQuantity(Base):
     __tablename__ = "comercialized_sub_categories_with_quantity"
     id = Column(Integer, primary_key=True, index=True)
-    subcategory_id = Column(Integer, ForeignKey("sub_categories.id"))
+    subcategory_id = ForeignKey("SubCategories.id")
     year = Column(Integer, nullable=False)
     quantity_l = Column(Integer, nullable=False)
 
@@ -60,7 +60,7 @@ class GrapeCategories(Base):
     __tablename__ = "grape_categories"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
-    variety_id = Column(Integer, ForeignKey("grape_varieties.id"))
+    variety_id = ForeignKey("GrapeVarieties.id")
     description = Column(String, nullable=False)
 
 
@@ -69,14 +69,14 @@ class GrapeSubCategories(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     description = Column(String, nullable=False)
-    category_id = Column(Integer, ForeignKey("grape_categories.id"))
+    category_id = ForeignKey("GrapeCategories.id")
 
 
 class ProcessedGrapes(Base):
     __tablename__ = "processed_grapes"
     id = Column(Integer, primary_key=True, index=True)
-    category_id = Column(Integer, ForeignKey("grape_categories.id"))
-    subcategory_id = Column(Integer, ForeignKey("grape_sub_categories.id"))
+    category_id = ForeignKey("GrapeCategories.id")
+    subcategory_id = ForeignKey("GrapeSubCategories.id")
     year = Column(Integer, nullable=False)
     quantity_kg = Column(Integer, nullable=False)
 
@@ -95,7 +95,7 @@ class CommonColumnsImportExport(Base):
     quantity_kg = Column(Integer, nullable=False)
     value_usdolars = Column(Integer, nullable=False)
     year = Column(Integer, nullable=False, index=True)
-    goods_id = Column(Integer, ForeignKey("goods_imported_exported.id"))
+    goods_id = ForeignKey("GoodsImportedExported.id")
 
 
 class Importacao(CommonColumnsImportExport):
