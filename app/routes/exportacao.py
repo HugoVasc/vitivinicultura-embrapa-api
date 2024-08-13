6b5e28fc9053a21b317273f89997d356da5c1bab
João Pedro da Cunha Dias <joaopedrocdias@hotmail.com>
2024-08-12 21:20:40 -0300

    updating exportacao and importacao routes

--- app/routes/exportacao.py
+++ app/routes/exportacao.py
@@ -84,9 +84,68 @@ async def read_exportacao(db: db_dependency, year: int = Path(..., ge=1970, le=2
     raise HTTPException(status_code=404, detail="Exportacao not found.")
 
 
-@router.post("/exported_goods", status_code=status.HTTP_201_CREATED)
+@router.post("/new_exported_goods", status_code=status.HTTP_201_CREATED)
 async def create_exported_good(exported_good: GoodsImportedExportedRequest, db: db_dependency):
     goods = GoodsImportedExported(**exported_good.model_dump(), exported=True)
     db.add(goods)
     db.commit()
     return goods
+
+
+@router.post("/new_exportacao", status_code=status.HTTP_201_CREATED)
+async def create_exportacao(exportacao: ExportacaoRequest, db: db_dependency):
+    goods = db.query(GoodsImportedExported).filter(GoodsImportedExported.id == exportacao.goods_id).first()
+    if goods is None:
+        raise HTTPException(status_code=404, detail="Goods not found.")
+    exportacao = Exportacao(**exportacao.model_dump())
+    db.add(exportacao)
+    db.commit()
+    return exportacao
+
+
+@router.put("/{exportacao_id}", status_code=status.HTTP_200_OK)
+async def update_exportacao(
+        exportacao_id: int,
+        exportacao_request: ExportacaoRequest,
+        db: db_dependency
+):
+    # Fetch the existing Exportacao entry
+    exportacao = db.query(Exportacao).filter(Exportacao.id == exportacao_id).first()
+
+    if not exportacao:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Exportacao not found.")
+
+    # Check if the goods_id exists in GoodsImportedExported
+    goods = db.query(GoodsImportedExported).filter(GoodsImportedExported.id == exportacao_request.goods_id).first()
+
+    if not goods:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Goods ID does not exist.")
+
+    # Update the fields of the Exportacao entry
+    exportacao.country = exportacao_request.country
+    exportacao.quantity_in_kg = exportacao_request.quantity_in_kg
+    exportacao.value_us_dollars = exportacao_request.value_us_dollars
+    exportacao.year = exportacao_request.year
+    exportacao.goods_id = exportacao_request.goods_id
+
+    # Commit the changes to the database
+    db.commit()
+    db.refresh(exportacao)
+
+    return exportacao
+
+
+@router.delete("/{exportacao_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_exportacao(exportacao_id: int, db: db_dependency):
+    # Fetch the existing Exportacao entry
+    exportacao = db.query(Exportacao).filter(Exportacao.id == exportacao_id).first()
+
+    if not exportacao:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Exportacao not found.")
+
+    # Delete the Exportacao entry
+    db.delete(exportacao)
+    db.commit()
+
+    # No content to return, just the status code 204
+    return
--- app/routes/importacao.py
+++ app/routes/importacao.py
@@ -1,83 +1,125 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException, Path, Query
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
 from starlette import status
-import sqlite3
+
+from ..database import SessionLocal
+from ..models import Importacao, GoodsImportedExported
 
 router = APIRouter(
     prefix="/importacao",
     tags=["importacao"],
 )
 
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+class ImportacaoRequest(BaseModel):
+    country: str
+    quantity_in_kg: float
+    value_us_dollars: float
+    year: int
+    goods_id: int
+
+
 @router.get("/", status_code=status.HTTP_200_OK)
-async def importacao():
-    """
-    Rota Default para /importacao
-    """
+async def read_all_importacoes(db: db_dependency, limit: int = Query(default=10, gt=0, lt=100)):
+    return db.query(Importacao).limit(limit).all()
 
-    return {
-        'message': 'OK',
-        'env': 'importacao'
-    }
 
-@router.get("/categoria/", status_code=status.HTTP_200_OK)
-def get_importacao_categoria(categoria: str):
-    """
-    Rota Importacao / dados por categoria
-    """
-    #Banco de Dados
-    try:
-        conn = sqlite3.connect('vitiVinicultura.db')
-        cur = conn.cursor()
-    except:
-        print("Erro na conexão com o banco de dados")    
-    
-    control = 'NA'
-    produto = 'NA'
+@router.get("/{importacao_id}", status_code=status.HTTP_200_OK)
+async def read_importacao(db: db_dependency, importacao_id: int = Path(gt=0), ):
+    importacao = db.query(Importacao).filter(Importacao.id == importacao_id).first()
+    if not importacao:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Importacao not found.")
+    return importacao
 
-    # Pegar o ID da Categoria
-    ret = []
-    goods_id = 0
-    try:
-        cur.execute(f"SELECT id FROM goods_imported_exported WHERE name='{categoria}'")
-        rows = cur.fetchall()
-        if len(rows) == 0:
-            ret.append({
-                'msg_goods': 'categoria nao encontrada'
-            })
-        else:
-            for row in rows:
-               goods_id = row[0]
-    except:
-        print("Erro na consulta goods_imported_exported")
-    
-    
-    # Conslulta na tabela importacao
-    try:
-        cur.execute(f"SELECT id,countries,quantity_kg,value_usdolars,year,goods_id FROM importacao WHERE goods_id='{goods_id}'")
-        rows = cur.fetchall()
 
-        if len(rows) == 0:
-            ret.append({
-                'msg_importacao': 'nenhum item encontrado'
-            })
-        else:
-            for row in rows:
-                country = row[1]
-                quantity_kg = row[2]
-                value_usdolars = row[3]
-                year = row[4]
-                goods_id = row[5]
-               
-                ret.append({ 
-                    'pais': country,
-                    'quantity_kg' : quantity_kg,
-                    'value_usdolars': value_usdolars,
-                    'categoria': categoria,
-                    'id_categoria': goods_id
-                })
+@router.get("/goods/{categoria}", status_code=status.HTTP_200_OK)
+async def get_importacao_by_categoria(categoria: str, db: db_dependency):
+    # Find the goods ID based on the category name
+    goods = db.query(GoodsImportedExported).filter(GoodsImportedExported.name == categoria).first()
+    if not goods:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Categoria not found.")
 
-    except:
-        print("Erro na consulta goods_imported_exported")    
-    
-    
-    return ret
-    
+    importacoes = db.query(Importacao).filter(Importacao.goods_id == goods.id).all()
+    if not importacoes:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No importacoes found for this category.")
+
+    return importacoes
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_importacao(importacao_request: ImportacaoRequest, db: db_dependency):
+    # Check if the goods_id exists in GoodsImportedExported
+    goods = db.query(GoodsImportedExported).filter(GoodsImportedExported.id == importacao_request.goods_id).first()
+
+    if not goods:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Goods ID does not exist.")
+
+    # Create the Importacao entry
+    importacao = Importacao(**importacao_request.model_dump())
+    db.add(importacao)
+    db.commit()
+    db.refresh(importacao)
+
+    return importacao
+
+
+@router.put("/{importacao_id}", status_code=status.HTTP_200_OK)
+async def update_importacao(
+        importacao_id: int,
+        importacao_request: ImportacaoRequest,
+        db: db_dependency
+):
+    # Fetch the existing Importacao entry
+    importacao = db.query(Importacao).filter(Importacao.id == importacao_id).first()
+
+    if not importacao:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Importacao not found.")
+
+    # Check if the goods_id exists in GoodsImportedExported
+    goods = db.query(GoodsImportedExported).filter(GoodsImportedExported.id == importacao_request.goods_id).first()
+
+    if not goods:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Goods ID does not exist.")
+
+    # Update the fields of the Importacao entry
+    importacao.country = importacao_request.country
+    importacao.quantity_in_kg = importacao_request.quantity_in_kg
+    importacao.value_us_dollars = importacao_request.value_us_dollars
+    importacao.year = importacao_request.year
+    importacao.goods_id = importacao_request.goods_id
+
+    # Commit the changes to the database
+    db.commit()
+    db.refresh(importacao)
+
+    return importacao
+
+
+@router.delete("/{importacao_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_importacao(importacao_id: int, db: db_dependency):
+    # Fetch the existing Importacao entry
+    importacao = db.query(Importacao).filter(Importacao.id == importacao_id).first()
+
+    if not importacao:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Importacao not found.")
+
+    # Delete the Importacao entry
+    db.delete(importacao)
+    db.commit()
+
+    # No content to return, just the status code 204
+    return
